c1bc23b39fb9ff905104193c464a814e0e4f3fef
Sanio <krasulazr@gmail.com>
2025-04-27 18:30:39 +0300

    fix bugs 2

--- components/member.tsx
+++ components/member.tsx
@@ -2,28 +2,36 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 
-interface Props{
+interface Props {
     id: string,
     name: string,
     socialId: string,
     link: string,
 }
 
-const Member: React.FC<Props> = ({id, name, socialId, link}) => {
-    return ( 
+const Member: React.FC<Props> = ({ id, name, socialId, link }) => {
+    return (
         <div>
-        <div>
-            <Image src={`/assets/members/margelo_faces_${name}.svg`} width={1366} height={1555} alt="members"/>
+            <div>
+                <Image
+                    src={`/assets/members/margelo_faces_${name}.svg`}
+                    width={1366}
+                    height={1555}
+                    alt={name}
+                    onError={(e) => {
+                        console.error('Failed to load image:', e);
+                    }}
+                />
+            </div>
+
+            <div className="text-2xl xl:text-3xl">{name}</div>
+            <div className="text-xl">
+                <Link href={link} target="_blank">
+                    {socialId}
+                </Link>
+            </div>
         </div>
-        
-        <div className="text-2xl xl:text-3xl">{name}</div>
-        <div className="text-xl">
-            <Link href={link} target="_blank">
-                {socialId}
-            </Link>
-        </div>
-        </div>
-     );
+    );
 }
- 
+
 export default Member;
